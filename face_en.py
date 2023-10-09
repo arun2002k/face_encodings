1ae42e6cc4fd4b2197dc76722b1d3f7d199b8e0e
Arun <85438554+arun2002k@users.noreply.github.com>
2023-10-09 19:17:51 +0530

    Add files via upload

---
+++ face_en.py
@@ -0,0 +1,44 @@
+import cv2
+import face_recognition
+
+# Initialize the webcam
+video_capture = cv2.VideoCapture(0)
+
+# Create a list to store face encodings
+known_face_encodings = []
+
+while True:
+    # Capture a frame from the webcam
+    ret, frame = video_capture.read()
+
+    # Find face locations in the current frame
+    face_locations = face_recognition.face_locations(frame)
+
+    # Encode the detected faces
+    for face_location in face_locations:
+        top, right, bottom, left = face_location
+        face_image = frame[top:bottom, left:right]
+        face_encodings = face_recognition.face_encodings(face_image)
+        
+        if len(face_encodings) > 0:
+            known_face_encodings.append(face_encodings[0])
+
+    # Display the video frame with face rectangles
+    for (top, right, bottom, left) in face_locations:
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+    cv2.imshow('Video', frame)
+
+    # Press 'q' to exit the video loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Save the face encodings to a text file
+with open('face_encodings.txt', 'w') as file:
+    for encoding in known_face_encodings:
+        encoding_str = ','.join(map(str, encoding))
+        file.write(encoding_str + '\n')
+
+# Release the video capture and close the OpenCV window
+video_capture.release()
+cv2.destroyAllWindows()
